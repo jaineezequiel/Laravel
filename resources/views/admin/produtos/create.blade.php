36cadbe2a6cb7dd1577d570fc100ce75cda426b6
Jaine Ezequiel <jaineezequiel@gmail.com>
2024-01-13 11:17:32 -0300

    adicionando o bootstrap e vite

--- resources/views/admin/produtos/create.blade.php
+++ resources/views/admin/produtos/create.blade.php
@@ -1,6 +1,11 @@
 <x-layout title="Novo produto">
 
     <form>
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="user_name">
+
+        <button type="button" class="btn btn-success">Success</button>
     </form>
 
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }} - Gerenciamento de estoque</title>
+    <title>{{ $title }} - Lista de compras</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js']);
 </head>
 <body>
 <h1>{{ $title }}</h1>
